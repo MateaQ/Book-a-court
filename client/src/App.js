975484bc86fc9f8c36491df84edd94101f989f15
Matea Grdić <mgrdic@unipu.hr>
2021-09-22 15:26:14 +0200

    bok

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
-
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <div className="App">
 <button className="btn btn-primary">Button</button>
+<Navbar/>
     </div>
   );
 }
--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -14,3 +14,4 @@ return(
 
 
 }
+export default Navbar
