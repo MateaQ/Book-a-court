f7d87b0fcc33e0e90d7ddd6561b95b5987fd13e5
Matea Grdić <mgrdic@unipu.hr>
2021-09-23 22:09:13 +0200

    Register

--- client/src/screens/Regsterscreen.js
+++ client/src/screens/Regsterscreen.js
@@ -4,7 +4,12 @@ function Regsterscreen(){
 
 <div>
 <h1>Registracija</h1>
+<div className="row justify-content-centar mt-5">
+<div className="col-md-5"></div>
+</div>
 
+
+<input type ="text" className="form-control" placleholder="email" />
 
 </div>
 
